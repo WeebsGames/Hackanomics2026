d094c1fc1aa830330565046b9150fe22c115a314
AreebJ <68392559+WeebsGames@users.noreply.github.com>
2026-03-04 14:48:21 -0600

    added racetrack + fixed wheels
    experimenting with friction values. trying to make the game not be awful while i add other elements

--- Assets/Scripts/WheelControl.cs
+++ Assets/Scripts/WheelControl.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using UnityEngine;
 
 public class WheelControl : MonoBehaviour
@@ -11,14 +12,25 @@ public class WheelControl : MonoBehaviour
     // Editor Inspector window)
     public bool steerable;
     public bool motorized;
+    public float aSlip = 0.8f;
+    public float aValue = 0.5f;
+    public float eSlip = 0.4f;
+    public float eValue = 1.0f;
 
     Vector3 position;
     Quaternion rotation;
+    WheelFrictionCurve newFriction;
 
     // Start is called before the first frame update
     private void Start()
     {
         WheelCollider = GetComponent<WheelCollider>();
+        newFriction.asymptoteSlip = aSlip;
+        newFriction.asymptoteValue = aValue;
+        newFriction.extremumSlip = eSlip;
+        newFriction.extremumValue = eValue;
+        WheelCollider.forwardFriction = newFriction;
+
     }
 
     // Update is called once per frame
@@ -28,7 +40,6 @@ public class WheelControl : MonoBehaviour
         // use them to set the wheel model's position and rotation
         WheelCollider.GetWorldPose(out position, out rotation);
         wheelModel.transform.position = position;
-
         wheelModel.transform.rotation = rotation;
     }
 }
